dcf6875d1b317d695a175cde50fe92ec0735e86b
Arya-Roy-Yowlap <arya.roy@yowlapro.com>
2025-05-31 16:10:59 -0700

    MT5 comparison 76

--- train_from_csv.py
+++ train_from_csv.py
@@ -159,34 +159,26 @@ class Game(object):
 
 
     def _get_last_N_timebars(self):
-        print(f"Current index: {self.curr_idx}, Lookback: {self.lkbk}")
-        
         # Get 30m bars
         if int(self.curr_idx) >= self.lkbk-1:
             self.last30m = self.bars30m.iloc[int(self.curr_idx)-self.lkbk+1:int(self.curr_idx)+1]
-            print(f"30m bars shape: {self.last30m.shape}")
         else:
             padding = self.bars30m.iloc[0].to_frame().T.repeat(self.lkbk - int(self.curr_idx) - 1)
             self.last30m = pd.concat([padding, self.bars30m.iloc[:int(self.curr_idx)+1]])
-            print(f"30m bars padded shape: {self.last30m.shape}")
             
         # Get 4h bars
         if int(self.curr_idx) >= self.lkbk-1:
             self.last4h = self.bars4h.iloc[int(self.curr_idx)-self.lkbk+1:int(self.curr_idx)+1]
-            print(f"4h bars shape: {self.last4h.shape}")
         else:
             padding = self.bars4h.iloc[0].to_frame().T.repeat(self.lkbk - int(self.curr_idx) - 1)
             self.last4h = pd.concat([padding, self.bars4h.iloc[:int(self.curr_idx)+1]])
-            print(f"4h bars padded shape: {self.last4h.shape}")
             
         # Get 1d bars
         if int(self.curr_idx) >= self.lkbk-1:
             self.last1d = self.bars1d.iloc[int(self.curr_idx)-self.lkbk+1:int(self.curr_idx)+1]
-            print(f"1d bars shape: {self.last1d.shape}")
         else:
             padding = self.bars1d.iloc[0].to_frame().T.repeat(self.lkbk - int(self.curr_idx) - 1)
             self.last1d = pd.concat([padding, self.bars1d.iloc[:int(self.curr_idx)+1]])
-            print(f"1d bars padded shape: {self.last1d.shape}")
 
     def get_state(self):
         self._assemble_state()
